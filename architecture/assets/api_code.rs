5c072e3d9d4ef3a865ead30bffe09537abd3a934
xphx <1324462+xphx@users.noreply.github.com>
2025-09-09 16:18:09 +0000

    Checkpoint

--- architecture/assets/api_code.rs
+++ architecture/assets/api_code.rs
@@ -6,15 +6,11 @@ let settings = RenderSettings {
 
 let mut ctx = RenderContext::new_with(100, 100, settings);
 
-// Draw a blue rectangle of width 50.
+// Draw different shapes in various colors.
 ctx.set_paint(BLUE);
 ctx.fill_path(&Rect::new(25.0, 25.0, 75.0, 75.0).to_path(0.1));
-
-// Draw another semi-transparent rectangle.
 ctx.set_paint(RED.with_alpha(0.5));
 ctx.fill_path(&Rect::new(50.0, 50.0, 85.0, 85.0).to_path(0.1));
-
-// Draw a stroked circle in the center with radius 30.
 ctx.set_paint(GREEN);
 ctx.stroke_path(&Circle::new((50.0, 50.0), 30.0).to_path(0.1));
 
@@ -22,6 +18,7 @@ ctx.stroke_path(&Circle::new((50.0, 50.0), 30.0).to_path(0.1));
 ctx.flush();
 
 let mut pixmap = Pixmap::new(100, 100);
+// Render the results to a pixmap.
 ctx.render_to_pixmap(&mut pixmap);
 
 // Encode the pixmap into a PNG file.
